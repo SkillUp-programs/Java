101f2bc7ee64e008bb5cfe47c4ca1a4b658e67dd
Technical hub <62637097+technicalhub9@users.noreply.github.com>
2020-08-30 22:39:52 +0530

    Add files via upload

---
+++ For1.java
@@ -0,0 +1,12 @@
+package Pack2;
+public class For1 {
+	public static void main(String[] args) {
+		System.out.println("5 table:");
+		int i = 1;
+		int n = 5;		
+		do
+		{			
+			System.out.println(n + " x " + i + " = "+ (n*i));
+			i = i+1;
+		}while(i<=10);
+}}
