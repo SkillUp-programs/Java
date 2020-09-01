f7b665f47f782d430710175d2c50f18bc0eaefdf
Technical hub <62637097+technicalhub9@users.noreply.github.com>
2020-09-01 19:06:01 +0530

    Add files via upload

--- File1.java
+++ File1.java
@@ -1,29 +1,11 @@
-package pack3;
-class A1  // composition or sub class
-{
-	public void sum(int x , int y)
-	{
-		System.out.println("sum = " + (x+y));
-	}
-}
-
-class B1
-{
-	public void sub(int a, int b)
-	{
-		A1 obj = new A1();
-		 obj.sum(20,10);
-		System.out.println("sub = "+ (a-b));
-	}
-}
-
-
-public class File1  // Aggregation or main class 
+package Pack5;
+public class File1 
 {
 	public static void main(String[] args) 
-	{	 
-	 B1 obj2 = new B1();
-	 obj2.sub(30, 10);
-	 System.out.println("Main class");
+	{
+	 int a =20;
+	 Integer i = Integer.valueOf(a); // converts int into integer
+	 Integer j = a; // autoboxing
+	 System.out.println("a = "+a+" i = "+i + " j = "+j);
 	}
 }
---
+++ File2.java
@@ -0,0 +1,13 @@
+package Pack5;
+public class File2 
+{
+	public static void main(String[] args) 
+	{
+	  //to store value in object
+		Integer a = new Integer(3);
+	// to convert object value into var value
+		int i = a.intValue();
+		int j = a; //unboxing
+		System.out.println("a = "+a+", i = "+i+", j = "+j);	
+	}
+}
