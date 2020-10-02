46ad2b2d1b4b11c902a3bc031e22b50d0f4dd705
kolagadisuresh1977 <71330112+kolagadisuresh1977@users.noreply.github.com>
2020-10-02 11:08:18 +0530

    Add files via upload

---
+++ MyThread2.java
@@ -0,0 +1,25 @@
+package Prgs7;
+
+class Thread2 implements Runnable
+{
+	public void run()
+	{
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("sub class");
+		}
+	}
+}
+
+public class MyThread2 {
+	public static void main(String[] args) {
+		Thread2 obj = new Thread2();
+		Thread obj2 = new Thread(obj);
+		obj2.start();
+		for(int j=1;j<=10;j++)
+		{
+			System.out.println("main class");
+		}
+	}
+
+}
