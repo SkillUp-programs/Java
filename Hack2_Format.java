ee7af657efac61d0db400a6fbda86d20c50d13ef
kolagadisuresh1977 <71330112+kolagadisuresh1977@users.noreply.github.com>
2020-10-12 11:12:22 +0530

    Add files via upload

---
+++ Hack2_Format.java
@@ -0,0 +1,17 @@
+package Hack1;
+import java.util.Scanner;
+public class Hack2_Format {
+	    public static void main(String[] args) {
+	            Scanner sc=new Scanner(System.in);
+	            System.out.println("================================");
+	            for(int i=0;i<3;i++){
+	                String s1=sc.next();
+	                int x=sc.nextInt();
+	                //Complete this line
+	                System.out.printf("%-15s%03d%n",s1,x);
+	            }
+	            System.out.println("================================");
+
+	    }
+	}
+
