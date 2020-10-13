941e60255ccb09442e7f65ebe5ced015c0c9d9d7
kolagadisuresh1977 <71330112+kolagadisuresh1977@users.noreply.github.com>
2020-10-13 13:00:25 +0530

    Add files via upload

--- Hack4_Datatypes.java
+++ Hack4_Datatypes.java
@@ -14,6 +14,12 @@ import java.util.*;
 	                System.out.println(x+" can be fitted in:");
 	                if(x>=-128 && x<=127)System.out.println("* byte");
 	                //Complete the code
+	                if(x >= -Math.pow(2, 15) && x <= Math.pow(2, 15) - 1)
+	                    System.out.println("* short");
+	                if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1)
+	                    System.out.println("* int");
+	                if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1)
+	                    System.out.println("* long");
 	            }
 	            catch(Exception e)
 	            {
