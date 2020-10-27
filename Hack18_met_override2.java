a68cdf1c14de6c06ab2427c0012aa80c2229cac9
kolagadisuresh1977 <71330112+kolagadisuresh1977@users.noreply.github.com>
2020-10-27 19:21:39 +0530

    Add files via upload

---
+++ Hack18_met_override2.java
@@ -0,0 +1,29 @@
+package Hack1;
+
+
+	import java.util.*;
+	import java.io.*;
+
+	class BiCycle{
+		String define_me(){
+			return "a vehicle with pedals.";
+		}
+	}
+
+	class MotorCycle extends BiCycle{
+		String define_me(){
+			return "a cycle with an engine.";
+		}
+		
+		MotorCycle(){
+			System.out.println("Hello I am a motorcycle, I am "+ define_me());
+			String temp=super.define_me(); //Fix this line
+			System.out.println("My ancestor is a cycle who is "+ temp );
+		}
+		
+	}
+	public class Hack18_met_override2 {
+		public static void main(String []args){
+			MotorCycle M=new MotorCycle();
+		}
+	}
---
+++ Hack19_Exp1.java
@@ -0,0 +1,27 @@
+package Hack1;
+	import java.io.*;
+	import java.util.*;
+	import java.text.*;
+	import java.math.*;
+	import java.util.regex.*;
+
+	public class Hack19_Exp1 {
+
+	    public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+
+	        
+	        Scanner scan = new Scanner(System.in);
+	        try {
+	            int x = scan.nextInt();
+	            int y = scan.nextInt();
+	            System.out.println(x/y);
+	        } catch (InputMismatchException e) {
+	            System.out.println(e.getClass().getName());
+	        } catch (ArithmeticException e) {
+	            System.out.println(e.getClass().getName() + ": / by zero");
+	        }
+	    }
+	}
+
